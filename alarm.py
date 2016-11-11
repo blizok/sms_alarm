c0caee2914e25d3087d06d85eaa1ecd0d95d5235
blizok <blizok@users.noreply.github.com>
2016-11-11 23:39:15 +0200

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,16 +1,14 @@
 import MOD
 import MDM
-
+import GPIO
+#sms sending function
 def sendSMS( number, smstext):
-
-		
 	if number=="" or smstext=="" : return 0
 	#text mode setup
 	MDM.send('AT+CMGF=1\r',2)
 	MDM.receive(20)
 	#Send SMS intialization
 	MDM.send('AT+CMGS="' + number + '"\r', 2)
-
 	#"<" waiting
 	res=0
 	n=0
@@ -28,11 +26,31 @@ def sendSMS( number, smstext):
 	while a=='':
 		a = MDM.receive(20)
 	return ( a.find('OK')!=-1 )
+#gpio handlilng function
+#def gpioRead():
+#v0 = 0
+v1 = 0
+#myText = ""
+#GPIO.setIOvalue(2,0)
+#GPIO.setIOdir(2,1,0)
+v1 = GPIO.getIOvalue(3)
+myText = str(v1)
+#was open
+#if v0 == 0 and v1 == 1:
+#	status = "the door was open"
+#was closed
+#elif v0 == 1 or v1 == 0:
+#	status = "the door was closed"
+#else :
+#	status = "business as usual"
+		
+
 
 
 #sms details
 myNumber = "+972544939984"
-myText = "mnjam"
+#myText = status 
+#checking alarm status
+#gpioRead()
 #Sending SMS
 sendSMS(myNumber,myText)
-
