f7956ef9410693e18826351ee94e13ca54a12018
blizok <blizok@users.noreply.github.com>
2016-11-11 17:53:55 +0200

    sms function - done

--- alarm.py
+++ alarm.py
@@ -1,37 +1,38 @@
 import MOD
 import MDM
 
-#def checkNetwork():
-#	MOD.sleep(20)
-#	REC_TIME = 200
-#	for _ in range(10):
-#		MDM.send("AT+CREG?\r",0)
-#		res = MDM.receive(REC_TIME)
-#		if (res.find('0,1')!=-1): return 1
-#		else: MOD.sleep(50)
-#	return 0
-
 def sendSMS( number, smstext):
+
+		
 	if number=="" or smstext=="" : return 0
+	#text mode setup
 	MDM.send('AT+CMGF=1\r',2)
 	MDM.receive(20)
-	a = MDM.send('AT+CMGS="' + number + '"\r', 2)
-	res = MDM.receive(10)          
-	a = MDM.send(smstext, 2)
-	a = MDM.sendbyte(0x1A, 2)
+	#Send SMS intialization
+	MDM.send('AT+CMGS="' + number + '"\r', 2)
+
+	#"<" waiting
+	res=0
+	n=0
+	while n<10:
+		res = MDM.receive(10)
+		if res.find(">")>-1:
+			break
+		else:
+			n=n+1	
+	#sms text inserting and ctrl+z confirming
+	MDM.send(smstext, 2)
+	MDM.sendbyte(0x1A, 2)
+	#"OK" confirmation waiting
 	a=''
 	while a=='':
 		a = MDM.receive(20)
 	return ( a.find('OK')!=-1 )
 
-#print "Start"
-#while not checkNetwork():
-#	print "No network"
-#	MOD.sleep(10)
-#print "I find network"
+
 
 myNumber = "+972544939984"
-myText = "Hello world"
+myText = "mnjam"
 #print "Trying to send SMS"
 if sendSMS(myNumber,myText):
 	b=1
