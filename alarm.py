1366391c6b088a53514f9fa5fbb9fb28097f2394
blizok <blizok@users.noreply.github.com>
2016-11-19 21:21:50 +0200

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,10 +1,5 @@
-import MOD
 import MDM
 import GPIO
-
-import time
-
-TIMEOUT_CMD = 20
 
 #sms sending function
 def sendSMS( number, smstext):
@@ -32,66 +27,33 @@ def sendSMS( number, smstext):
 		a = MDM.receive(20)
 	return ( a.find('OK')!=-1 )
 
-
-
-
-
-def getIOvalue(GPIOnumber):
-  global MDM
-  if MDM.mdmser.getDCD() == 0:
-    MDM.mdmser.send('AT#GPIO=',0)
-    MDM.mdmser.send(str(GPIOnumber),0)
-    MDM.mdmser.send(',2\r',0)
-    timer = time.time() + TIMEOUT_CMD
-    resp = MDM.mdmser.read()
-    while((resp.find('OK') == -1) and (resp.find('ERROR') == -1) and (time.time() < timer)):
-      #time.sleep(0.1)
-      resp = resp + MDM.mdmser.read()
-
-    if resp.find('ERROR') != -1:
-      result = -1
-    else:
-      commapos = resp.find(',')
-      stat = resp[commapos+1]
-      result = int(stat)
-  else:
-    print 'dummy getIOvalue(', GPIOnumber, ')'
-    result = -1
-  return result
-
-v1 = getIOvalue(3)
-
-
-
-#gpio handlilng function
-#def gpioRead():
-#v0 = 0
-#myText = ""
-#GPIO.setIOvalue(2,0)
-#GPIO.setIOdir(2,1,0)
-#v1 = GPIO.getIOvalue(3)
-#myText = str(v1)
-#was open
-#if v0 == 0 and v1 == 1:
-#	status = "the door was open"
-#was closed
-#elif v0 == 1 or v1 == 0:
-#	status = "the door was closed"
-#else :
-#	status = "business as usual"
-		
-
-
-
-#sms details
+#initial door status check and sms
+myText = "Garazhik alarm system is up and running"
 myNumber = "+972544939984"
-myText = str(v1)
-#myText = status 
-#checking alarm status
-#gpioRead()
-#Sending SMS
-#sendSMS(myNumber,myText)
-
-#while (1):
-#	if(GPIO.getIOvalue(3)==0):
 sendSMS(myNumber,myText)
+
+door2 = GPIO.getIOvalue(10)
+if door2 == 0:
+	myText = "Door status:closed"
+elif door2 == 1:
+	myText = "Door status:open"
+else:
+	myText = "Something went wrong on boot..."
+myNumber = "+972544939984"
+sendSMS(myNumber,myText)
+door1 = door2
+
+#endless loop with door lookup
+while (1):
+	door2 = GPIO.getIOvalue(10)
+	if door1 != door2:
+		if door2 == 0:
+			myText = "The door was closed"
+		elif door2 == 1:
+			myText = "The door was opened"
+		else:
+			myText = "Something went wrong..."
+		door1 = door2
+		myNumber = "+972544939984"
+		sendSMS(myNumber,myText)
+
