f143b68e5c8c4abcd30c9e1e836ea3002ca159fd
blizok <blizok@users.noreply.github.com>
2016-11-21 02:01:16 +0200

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,8 +1,11 @@
 import MDM
 import GPIO
 
+allowedNumbers = ['+972544939984','+972543042360']
+myNumber = "+972544939984"
+
 #sms sending function
-def sendSMS( number, smstext):
+def sendSMS(number,smstext):
 	if number=="" or smstext=="" : return 0
 	#text mode setup
 	MDM.send('AT+CMGF=1\r',2)
@@ -27,11 +30,14 @@ def sendSMS( number, smstext):
 		a = MDM.receive(20)
 	return ( a.find('OK')!=-1 )
 
-#initial door status check and sms
-myText = "Alarm system is up and running"
-myNumber = "+xxx"
+
+#send SMS on boot
+myText = "Garazhik alarm system is up and running"
 sendSMS(myNumber,myText)
 
+#initial door status check and sms
+door1 = 0
+door2 = 0
 door2 = GPIO.getIOvalue(10)
 if door2 == 0:
 	myText = "Door status:closed"
@@ -39,7 +45,6 @@ elif door2 == 1:
 	myText = "Door status:open"
 else:
 	myText = "Something went wrong on boot..."
-myNumber = "+xxx"
 sendSMS(myNumber,myText)
 door1 = door2
 
@@ -54,6 +59,38 @@ while (1):
 		else:
 			myText = "Something went wrong..."
 		door1 = door2
-		myNumber = "+xxx"
 		sendSMS(myNumber,myText)
 
+	#reset the module or get the current door status with an sms massage
+	#valid number verification
+	MDM.send('AT+CMGF=1\r',1)
+	resp = MDM.receive(1)
+	MDM.send('AT+CMGL="REC UNREAD"\r',1)
+	resp = MDM.receive(1)
+	respErr = resp.find('REC UNREAD')
+	if respErr > -1:
+		veriNumber = resp[respErr+13:respErr+26]
+		while veriNumber in allowedNumbers:
+			# look for a valid command
+			doReset = resp.find('reset')
+			doStatus = resp.find('status')
+			if doReset > -1:
+				myText="Performing the reset now upon sms request from " + veriNumber +"."
+				sendSMS(myNumber,myText)
+				MOD.sleep(5)
+				MDM.send('AT#REBOOT\r',2)
+			if doStatus > -1:
+				door2 = GPIO.getIOvalue(10)
+				if door2 == 0:
+					doorStatus = "Door status:closed"
+				elif door2 == 1:
+					doorStatus = "Door status:open"
+				else:
+					doorStatus = "I don't know..."
+				myText="Status requested by " + veriNumber + ". " + doorStatus + "" 
+		while veriNumber not in allowedNumbers:
+			myText="You are not authorized, fuck you."	
+		sendSMS(myNumber,myText)
+	MDM.send('AT+CMGD=1,4\r',1)
+
+#MOD.sleep(10)
